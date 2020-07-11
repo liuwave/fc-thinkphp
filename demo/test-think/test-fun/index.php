db19e3418bda3c17fb0d198a61f8d472312e9ab0
laywave <laywave@qq.com>
2020-07-11 10:11:57 +0800

    优化入口页面-增加Initializer函数 优化教程

--- demo/test-think/test-fun/index.php
+++ demo/test-think/test-fun/index.php
@@ -10,11 +10,39 @@ function initializer($context) {
     echo 'initializing' . PHP_EOL;
 }
 */
+$GLOBALS[ 'fcThink' ]=null;
 
+/**
+ * @param $request
+ * @param $context
+ *
+ * @return \RingCentral\Psr7\Response
+ * @throws \Exception
+ */
 function handler($request, $context) : Response
 {
+    if(!$GLOBALS['fcThink']){
+        $GLOBALS['fcThink']=getHandler($context);
+    }
+    return $GLOBALS[ 'fcThink' ]->setFcRequest($request)
+      ->run();
+}
+
+/**
+ * @param $context
+ */
+function initializer($context){
+
+    $GLOBALS[ 'fcThink' ] = getHandler($context);
+}
+
+/**
+ * @param $context
+ *
+ * @return \liuwave\fc\think\FcThink
+ */
+function getHandler($context){
     $appPath = __DIR__.'/tp';
     require $appPath.'/vendor/autoload.php';
-    
-    return (new FcThink($request, $context,['root'=> $appPath, 'runtime_path'=>'/tmp/']))->run();
+    return new FcThink(null, $context, ['root' => $appPath, 'runtime_path' => '/tmp/', 'is_cli' => false]);
 }
